1c98f9a13a87957be82c1f152d12a5eb1f31c82c
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-10-30 20:30:25 +0100

    made navbar

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,12 +1,27 @@
 const navbar = document.querySelector(".navbar");
 
 navbar.innerHTML = `<div class="navContainer">
+<div class="navToggle"><ion-icon name="menu-outline" class="menu"></ion-icon></div>
 <div class="logoDiv"><img class="logoImg" src="images/logo.png" alt="Logo"/></div>
-
-<div class="navList">
+<div class="navList showNavList">
 <a href="html/blog.html">Posts</a>
-<a href="html/about.html">About me</a>
-<a href="html/contact.html">Contact me</a></div>
-<div><button class="button-main">Log in</button></div>
+<a href="html/about.html">About</a>
+<a href="html/contact.html">Contact</a></div>
+<div><button class="button-main sizeBtn">Log in</button></div>
 
 </div>`;
+
+const menuToggle = document.querySelector(".menu");
+const navList = document.querySelector(".navList");
+
+menuToggle.addEventListener("click", () => {
+  if (navList.classList.contains("hideNavList")) {
+    // If navList is hidden, show it
+    navList.classList.remove("hideNavList");
+    navList.classList.add("showNavList");
+  } else {
+    // If navList is visible, hide it
+    navList.classList.remove("showNavList");
+    navList.classList.add("hideNavList");
+  }
+});

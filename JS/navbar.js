d56b6e31f0ee207a7d08e7e2c80de98c7ed77bff
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-10-30 13:07:20 +0100

    working on navbar

---
+++ JS/navbar.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector(".navbar");
+
+navbar.innerHTML = `<div class="navContainer">
+<div class="logoDiv"><img class="logoImg" src="images/logo.png" alt="Logo"/></div>
+
+<div class="navList">
+<a href="html/blog.html">Posts</a>
+<a href="html/about.html">About me</a>
+<a href="html/contact.html">Contact me</a></div>
+<div><button class="button-main">Log in</button></div>
+
+</div>`;

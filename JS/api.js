730988122ec702ce9663e1d27938f5dfb0a39dc1
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-11-05 20:52:10 +0100

    added carousel function to the front page

--- JS/api.js
+++ JS/api.js
@@ -49,6 +49,41 @@ async function displayPosts() {
       `;
       carouselContainer.appendChild(postCard);
     });
+    const carousel = document.querySelector(".carousel");
+    const prevButton = document.getElementById("slide-arrow-prev");
+    const nextButton = document.getElementById("slide-arrow-next");
+    const numVisiblePosts = 4;
+    let currentSlide = 0;
+
+    prevButton.addEventListener("click", () => {
+      if (currentSlide > 0) {
+        currentSlide--;
+        updateCarouselView();
+      }
+    });
+
+    nextButton.addEventListener("click", () => {
+      if (currentSlide < Math.ceil(posts.length / numVisiblePosts) - 1) {
+        currentSlide++;
+        updateCarouselView();
+      }
+    });
+
+    function updateCarouselView() {
+      const startIndex = currentSlide * numVisiblePosts;
+      const endIndex = startIndex + numVisiblePosts;
+      const postCards = carousel.querySelectorAll(".postCard");
+
+      postCards.forEach((card) => {
+        card.style.display = "none";
+      });
+
+      for (let i = startIndex; i < Math.min(endIndex, postCards.length); i++) {
+        postCards[i].style.display = "block";
+      }
+    }
+
+    updateCarouselView();
   } catch (error) {
     showError("Failed to fetch posts");
   }

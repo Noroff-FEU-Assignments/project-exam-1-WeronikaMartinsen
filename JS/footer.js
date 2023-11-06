dd415cf2692930f6f4e1dc9b09c9e16fc37f488e
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-11-06 12:01:51 +0100

    added footer

---
+++ JS/footer.js
@@ -0,0 +1,20 @@
+const footer = document.querySelector(".footer");
+
+footer.innerHTML = `<div class="footerContainer">
+<div class="footerBox footer-gap">
+<ion-icon id="footerIcon" name="logo-facebook"></ion-icon>
+<ion-icon id="footerIcon" name="logo-instagram"></ion-icon>
+<ion-icon id="footerIcon" name="logo-google"></ion-icon>
+<ion-icon id="footerIcon" name="logo-snapchat"></ion-icon>
+<ion-icon id="footerIcon" name="wifi-outline"></ion-icon>
+</div>
+<h4>Personal projects:</h4>
+<div class="footerBox gap">
+<a class="netlifyLink" href="https://classy-rabanadas-8c360c.netlify.app/">Rainy Days</a>
+<a class="netlifyLink" href="https://symphonious-marigold-52e292.netlify.app/">Museum</a>
+<a class="netlifyLink" href="https://venerable-douhua-2f5f5c.netlify.app/">Tesla</a>
+</div>
+
+<h4>WERONIKA MARTINSEN</h4>
+<span class="copyright">© Copyright 2023</span>
+</div>`;

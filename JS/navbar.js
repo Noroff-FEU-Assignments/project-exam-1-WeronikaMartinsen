d5efd4f1fac6d597e685e64857384e26b9ec55a0
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-11-09 11:18:47 +0100

    checking error

--- JS/navbar.js
+++ JS/navbar.js
@@ -4,10 +4,10 @@ navbar.innerHTML = `<div class="navContainer">
 <div class="navToggle"><ion-icon name="menu-outline" class="menu"></ion-icon></div>
 <a href="/index.html" class="logoDiv"><img class="logoImg"src="/images/logo.png" alt="Logo"/></a>
 <div class="navList hideNavList">
-<a href="/html/blog.html">Posts</a>
-<a href="/html/about.html">About</a>
-<a href="/html/contact.html">Contact</a></div>
-<div><button class="button-main sizeBtn">Log in</button></div>
+<a href="/html/blog.html">posts</a>
+<a href="/html/about.html">about me</a>
+<a href="/html/contact.html">contact</a></div>
+<div><button class="button-main sizeBtn">Chat me</button></div>
 
 </div>`;
 

4c47d839e2b67c73f836ace1531fe950049f3332
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-11-20 13:10:57 +0100

    working on navbar

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,7 +1,6 @@
 const navbar = document.querySelector(".navbar");
 
 navbar.innerHTML = `<div class="navContainer sticky">
-<div class="navToggle"><ion-icon name="menu-outline" class="menu"></ion-icon></div>
 <a href="/index.html" class="logoDiv"><img class="logoImg"src="/images/newLogo.png" alt="My personal "web&dev" logo."/></a>
 <div class="navList">
 <ul class="linkList">
@@ -9,8 +8,7 @@ navbar.innerHTML = `<div class="navContainer sticky">
 <li><a href="/html/blog.html">Blog</a></li>
 <li><a href="/html/about.html">About</a></li>
 <li><a class="btn-nav" href="/html/contact.html">Contact me</a></li>
-
-</ul></div>
+</ul></div><div class="navToggle"><ion-icon name="menu-outline" class="menu"></ion-icon></div>
 </div>`;
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".navToggle");

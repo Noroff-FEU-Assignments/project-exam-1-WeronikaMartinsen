ce2703eb21db12bd74dec7b61da84f537f12d245
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-11-29 12:50:04 +0100

    fixed contrast error

--- JS/navbar.js
+++ JS/navbar.js
@@ -7,7 +7,7 @@ navbar.innerHTML = `<div class="navContainer sticky">
 <li><a href="/index.html">Home</a></li>
 <li><a href="/html/blog.html">Blog</a></li>
 <li><a href="/html/about.html">About</a></li>
-<li><a id="navLinkColor"class="btn-nav" href="/html/contact.html">Contact me</a></li>
+<li><a class="btn-nav" href="/html/contact.html">Contact me</a></li>
 </ul></div><div class="navToggle"><ion-icon name="menu-outline" class="menu"></ion-icon></div>
 </div>`;
 document.addEventListener("DOMContentLoaded", function () {

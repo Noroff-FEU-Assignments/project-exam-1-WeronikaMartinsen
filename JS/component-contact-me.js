ce77175793f867f12eecca2dad221f00ea3e44f5
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-12-01 20:25:45 +0100

    added component I delated by mistake

---
+++ JS/component-contact-me.js
@@ -0,0 +1,5 @@
+const componentContactMe = document.querySelector(".componentContactMe");
+componentContactMe.innerHTML = `<h3 class="fontSize">Contact Me</h3>
+<span>Feel free to reach out if you have any questions or inquiries.</span>
+<span class="span-color">wb3167@gmail.com</span>
+<button class="btnCTA colorPink">Send Message</button>`;
